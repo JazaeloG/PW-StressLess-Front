aa763fdc0f99b5cfb9dbb277a6a9b59ae6f968d2
NataliaLM-26 <t.natalialm@gmail.com>
2024-12-17 19:36:21 -0600

    pruebas login

--- src/app/components/login/login.page.spec.ts
+++ src/app/components/login/login.page.spec.ts
@@ -4,33 +4,39 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RegistroService } from 'src/app/servicios/registro.service';
 import { of, throwError } from 'rxjs';
-import { IonicModule, IonInput } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
   let registroService: jasmine.SpyObj<RegistroService>;
   let router: jasmine.SpyObj<Router>;
+  let navController: NavController;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     const registroServiceSpy = jasmine.createSpyObj('RegistroService', ['loginUsuario']);
     const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
-    TestBed.configureTestingModule({
+    await TestBed.configureTestingModule({
       declarations: [LoginPage],
       imports: [FormsModule,IonicModule.forRoot(), ReactiveFormsModule],
       providers: [
         FormBuilder,
         { provide: RegistroService, useValue: registroServiceSpy },
-        { provide: Router, useValue: routerSpy }
-      ]
-    });
+        { provide: Router, useValue: routerSpy },
+        { provide: NavController, useValue: {navigateForward: jasmine.createSpy('navigateForward'),
+        },
+      }
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     registroService = TestBed.inject(RegistroService) as jasmine.SpyObj<RegistroService>;
     router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    navController = TestBed.inject(NavController);
     //fixture.detectChanges();
   });
 
@@ -44,7 +50,6 @@ describe('LoginPage', () => {
 
   it('should initialize loginForm on ngOnInit', () => {
     component.ngOnInit();
-
     expect(component.loginForm).toBeDefined();
     expect(component.loginForm.controls['usuario_Correo']).toBeDefined();
     expect(component.loginForm.controls['usuario_Password']).toBeDefined();
@@ -54,18 +59,13 @@ describe('LoginPage', () => {
     const mockResponse = { access_token: 'mockToken123' };
     spyOn(localStorage, 'setItem');
     component.ngOnInit();
-
-    // Configuramos el formulario válido
     component.loginForm.setValue({ usuario_Correo: 'test@correo.com', usuario_Password: '1234' });
     registroService.loginUsuario.and.returnValue(of(mockResponse));
-
     component.onSubmit();
-
     expect(registroService.loginUsuario).toHaveBeenCalledWith({
       usuario_Correo: 'test@correo.com',
       usuario_Password: '1234',
     });
-
     expect(localStorage.setItem).toHaveBeenCalledWith('correo', 'test@correo.com');
     expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', 'mockToken123');
     expect(router.navigate).toHaveBeenCalledWith(['/home']);
@@ -75,12 +75,9 @@ describe('LoginPage', () => {
     const mockError = new Error('Login error');
     spyOn(console, 'error');
     component.ngOnInit();
-
     component.loginForm.setValue({ usuario_Correo: 'test@correo.com', usuario_Password: '1234' });
     registroService.loginUsuario.and.returnValue(throwError(() => mockError));
-
     component.onSubmit();
-
     expect(console.error).toHaveBeenCalledWith('Error en el login:', mockError);
     expect(router.navigate).not.toHaveBeenCalled();
   });
